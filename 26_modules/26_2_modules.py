f93a93ebf8a76a1bea3f663d63bc32aa0b4827cd
Rahul-Encoded <rahulm03@outlook.com>
2025-01-23 18:12:38 +0530

    Add module examples with mathematical functions and usage demonstration

---
+++ 26_modules/26_2_modules.py
@@ -0,0 +1,16 @@
+import examples as ex
+
+result = ex.pi
+print(result)
+ex.newLine()
+result = ex.square(3)
+print(result)
+ex.newLine()
+result = ex.cube(3)
+print(result)
+ex.newLine()
+result = ex.circumference(3)
+print(result)
+ex.newLine()
+result = ex.area(3)
+print(result)
